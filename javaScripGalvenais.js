39323e4f5b5a164e07017dcadc01034f2664e278
crepze21 <creepedude@gmail.com>
2024-04-05 17:56:57 +0300

    menešu dienu skaits

--- javaScripGalvenais.js
+++ javaScripGalvenais.js
@@ -9,13 +9,26 @@ window.addEventListener("load", () => {
         const menesi = today.getMonth()
         const dienas = today.getDay()
         
+
         const stunda = stundas < 10 ? "0" + stundas : stundas;
         const minute = minutes < 10 ? "0" + minutes : minutes;
         const sekunde = sekundes < 10 ? "0" + sekundes : sekundes;
+        const menesuDienuSkaits = {
+            janvaris : 31,
+            februaris : 28,
+            marts : 31,
+            aprilis : 30,
+            maijs : 31,
+            junijs : 30,
+            julijs : 31,
+            augusts : 31,
+            septembris : 30,
+            oktobris : 31,
+            novembris : 30,
+            decembris : 31}
         
-        const
         
-        //let istaisLaiks = `${stunda}:${minute}:${sekunde}`
+        //let istaisLaiks = `${stunda}:${minute}:${sekunde}
         document.getElementById("stundas").innerHTML = stunda;
         document.getElementById("minutes").innerHTML = minute;
         document.getElementById("sekundes").innerHTML = sekunde;
