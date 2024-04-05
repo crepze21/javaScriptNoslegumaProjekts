d2f436f4592a276b99fc1983ead45b694ac39f3f
crepze21 <creepedude@gmail.com>
2024-04-05 11:11:14 +0300

    kkas ar datumiem

--- javaScripGalvenais.js
+++ javaScripGalvenais.js
@@ -1,5 +1,31 @@
-function datums(){
+window.addEventListener("load", () => {
+    pulkstenis()
+    function pulkstenis(){
+        const today = new Date()
+        const stundas = today.getHours()
+        const minutes = today.getMinutes()
+        const sekundes = today.getSeconds()
+        const gadi = today.getFullYear()
+        const menesi = today.getMonth()
+        const dienas = today.getDay()
+        
+        const stunda = stundas < 10 ? "0" + stundas : stundas;
+        const minute = minutes < 10 ? "0" + minutes : minutes;
+        const sekunde = sekundes < 10 ? "0" + sekundes : sekundes;
+        
+        const
+        
+        //let istaisLaiks = `${stunda}:${minute}:${sekunde}`
+        document.getElementById("stundas").innerHTML = stunda;
+        document.getElementById("minutes").innerHTML = minute;
+        document.getElementById("sekundes").innerHTML = sekunde;
 
+        setTimeout(pulkstenis, 1000);
+    }
+  })
+
+function datums(){
+    
 }
 function laiks(){
     
