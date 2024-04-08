880200787480502e935878baa92098551e6b2281
crepze21 <creepedude@gmail.com>
2024-04-08 14:10:50 +0300

    neprotu apturēt laiku

--- javaScripGalvenais.js
+++ javaScripGalvenais.js
@@ -29,17 +29,22 @@ window.addEventListener("load", () => {
         const menesuDienuSkaits = [31,februarisParaNepara,31,30,31,30,31,31,30,31,30,31]
         
         
+        ievaditaisLaiks = laiks()
+        console.log(ievaditaisLaiks)
         
-        document.getElementById("stundas").innerHTML = stunda;
-        document.getElementById("minutes").innerHTML = minute;
-        document.getElementById("sekundes").innerHTML = sekunde;
+        document.getElementById("stundas").innerHTML = ievaditaisLaiks[0] - stunda
+        document.getElementById("minutes").innerHTML = ievaditaisLaiks[1] - minute
+        document.getElementById("sekundes").innerHTML = 60 - sekunde
 
+ 
 
+        
 
  
         
 
         setTimeout(pulkstenis, 1000);
+
     }
   })
 
@@ -47,21 +52,43 @@ function datums(){
     let ievaditaisDatums
     const neapstradatsDatums = datumsIevade.value
     //diena, mēnesis, gads
+    if (isNaN(neapstradatsDatums)){
     const ievaditaisStringDatums = neapstradatsDatums.split(":")
+    let pagaiduCipars
 
-    return  ievaditaisDatums = ievaditaisStringDatums.map(function(numurs){return parseInt(numurs, 10)})
+    ievaditaisDatums = ievaditaisStringDatums.map(function(numurs){return parseInt(numurs, 10)})
+   
+   if(ievaditaisDatums[1] == 00 || ievaditaisDatums[1] == 0)
+   {
+        if(ievaditaisDatums[1] > 60 || ievaditaisDatums[0] > 24){
+            alert(`Nepareizi ievadīts laiks, lūdzu meiģiniet vēlreiz`)
+            }
+        else{
+            ievaditaisDatums.pop()
+            ievaditaisDatums.push(60)
+        }
+        
+    }   
+    else{
+        return ievaditaisDatums = [0,0,0]
+    }
     
-    
-}
+}}
 
 
 
 function laiks(){
     let ievaditaisLaiks
     const neapstradatsLaiks = laiksIevade.value
+    if (isNaN(neapstradatsLaiks)){
+        const ievaditaisStringLaiks = neapstradatsLaiks.split(":")
+        return ievaditaisLaiks = ievaditaisStringLaiks.map(function(numurs){return parseInt(numurs, 10)})
+    }
+    else{
+        return ievaditaisLaiks = [0,0]
+    }
 
-    const ievaditaisStringLaiks = neapstradatsLaiks.split(":")
-    return ievaditaisLaiks = ievaditaisStringLaiks.map(function(numurs){return parseInt(numurs, 10)})
+    
 }
 
 function nakamaLapa(){
